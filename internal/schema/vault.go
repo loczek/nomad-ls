df3c7c95029c5bd0e8a6f1dac72b18c72debe011
loczek <30776250+loczek@users.noreply.github.com>
2025-12-14 18:06:25 +0100

    fix more broken links

--- internal/schema/vault.go
+++ internal/schema/vault.go
@@ -9,13 +9,13 @@ import (
 var VaultSchema = &schema.BodySchema{
 	Attributes: map[string]*schema.AttributeSchema{
 		"allow_token_expiration": {
-			Description:  lang.Markdown("Specifies that Nomad clients should not attempt to renew a task's Vault token, allowing it to expire. This should only be used when a secret is requested from Vault once at the start of a task or in a short-lived prestart task. Long-running tasks should never set `allow_token_expiration=true` if they obtain Vault secrets via `template` blocks, as the Vault token will expire and the template runner will continue to make failing requests to Vault until its [`vault_retry`](/nomad/docs/configuration/client#vault_retry) attempts are exhausted, at which point the task will fail.\n\nWhen Nomad has been configured to use [Workload Identity with Vault](/nomad/docs/secure/vault/acl#nomad-workload-identities), Nomad clients will automatically detect when tokens cannot be refreshed (for example, when the Vault auth method is configured to issue batch tokens). In this case, the `allow_token_expiration` option will be implicitly set to `true` by the client. The [legacy Vault authentication workflow](/nomad/docs/v1.8.x/integrations/vault/acl) cannot automatically detect this."),
+			Description:  lang.Markdown("Specifies that Nomad clients should not attempt to renew a task's Vault token, allowing it to expire. This should only be used when a secret is requested from Vault once at the start of a task or in a short-lived prestart task. Long-running tasks should never set `allow_token_expiration=true` if they obtain Vault secrets via `template` blocks, as the Vault token will expire and the template runner will continue to make failing requests to Vault until its [`vault_retry`](https://developer.hashicorp.com/nomad/docs/configuration/client#vault_retry) attempts are exhausted, at which point the task will fail.\n\nWhen Nomad has been configured to use [Workload Identity with Vault](https://developer.hashicorp.com/nomad/docs/secure/vault/acl#nomad-workload-identities), Nomad clients will automatically detect when tokens cannot be refreshed (for example, when the Vault auth method is configured to issue batch tokens). In this case, the `allow_token_expiration` option will be implicitly set to `true` by the client. The [legacy Vault authentication workflow](https://developer.hashicorp.com/nomad/docs/v1.8.x/integrations/vault/acl) cannot automatically detect this."),
 			DefaultValue: &schema.DefaultValue{Value: cty.BoolVal(false)},
 			Constraint:   &schema.LiteralType{Type: cty.Bool},
 			IsOptional:   true,
 		},
 		"change_mode": {
-			Description:  lang.Markdown("Specifies the behavior Nomad should take if the Vault token changes. The possible values are:\n\n- [`\"noop\"`](/nomad/docs/job-specification/vault#noop) - take no action (continue running the task)\n- [`\"restart\"`](/nomad/docs/job-specification/vault#restart) - restart the task\n- [`\"signal\"`](/nomad/docs/job-specification/vault#signal) - send a configurable signal to the task"),
+			Description:  lang.Markdown("Specifies the behavior Nomad should take if the Vault token changes. The possible values are:\n\n- [`\"noop\"`](https://developer.hashicorp.com/nomad/docs/job-specification/vault#noop) - take no action (continue running the task)\n- [`\"restart\"`](https://developer.hashicorp.com/nomad/docs/job-specification/vault#restart) - restart the task\n- [`\"signal\"`](https://developer.hashicorp.com/nomad/docs/job-specification/vault#signal) - send a configurable signal to the task"),
 			DefaultValue: &schema.DefaultValue{Value: cty.StringVal("restart")},
 			Constraint:   &schema.LiteralType{Type: cty.String},
 			IsOptional:   true,
@@ -28,7 +28,7 @@ var VaultSchema = &schema.BodySchema{
 		},
 		// TODO: mark as enterprise only
 		"cluster": {
-			Description:  lang.Markdown("Specifies the Vault cluster to use. The Nomad client will retrieve a Vault token from the cluster configured in the agent configuration with the same [`vault.name`](/nomad/docs/configuration/vault#name). In Nomad Community Edition, this field is ignored."),
+			Description:  lang.Markdown("Specifies the Vault cluster to use. The Nomad client will retrieve a Vault token from the cluster configured in the agent configuration with the same [`vault.name`](https://developer.hashicorp.com/nomad/docs/configuration/vault#name). In Nomad Community Edition, this field is ignored."),
 			DefaultValue: &schema.DefaultValue{Value: cty.StringVal("default")},
 			Constraint:   &schema.LiteralType{Type: cty.String},
 			IsOptional:   true,
@@ -54,7 +54,7 @@ var VaultSchema = &schema.BodySchema{
 			IsOptional:   true,
 		},
 		"role": {
-			Description:  lang.Markdown("Specifies the Vault role used when retrieving a token from Vault using JWT and workload identity. If not specified the client's [`create_from_role`](/nomad/docs/configuration/vault#create_from_role) value is used."),
+			Description:  lang.Markdown("Specifies the Vault role used when retrieving a token from Vault using JWT and workload identity. If not specified the client's [`create_from_role`](https://developer.hashicorp.com/nomad/docs/configuration/vault#create_from_role) value is used."),
 			DefaultValue: &schema.DefaultValue{Value: cty.StringVal("")},
 			Constraint:   &schema.LiteralType{Type: cty.String},
 			IsOptional:   true,
