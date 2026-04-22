4e802f91aeb4086df210e7279eff90d28d1754fd
loczek <30776250+loczek@users.noreply.github.com>
2026-04-22 04:08:31 +0200

    refactor: trim `v` prefix from version

--- internal/lsp/handlers.go
+++ internal/lsp/handlers.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"runtime/debug"
+	"strings"
 
 	"github.com/hashicorp/hcl-lang/decoder"
 	"github.com/hashicorp/hcl-lang/lang"
@@ -27,7 +28,7 @@ func (s *Service) HandleInitialize(ctx context.Context, params *protocol.Initial
 	return &protocol.InitializeResult{
 		ServerInfo: &protocol.ServerInfo{
 			Name:    "nomad-ls",
-			Version: info.Main.Version,
+			Version: strings.TrimPrefix(info.Main.Version, "v"),
 		},
 		Capabilities: protocol.ServerCapabilities{
 			CompletionProvider: &protocol.CompletionOptions{},
