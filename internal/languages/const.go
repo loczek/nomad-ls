792cc6fff0b0d066ac904f89c9cdf823dafd08cb
loczek <30776250+loczek@users.noreply.github.com>
2026-04-26 16:47:13 +0200

    feat: add more languages

--- internal/languages/const.go
+++ internal/languages/const.go
@@ -3,9 +3,14 @@ package languages
 type LanguageID string
 
 const (
-	NomadJob   LanguageID = "nomad-job"
-	NomadAgent LanguageID = "nomad-agent"
-	NomadVar   LanguageID = "nomad-var"
+	NomadJob           LanguageID = "nomad-job"
+	NomadAgent         LanguageID = "nomad-agent"
+	NomadACL           LanguageID = "nomad-acl"
+	NomadNapespace     LanguageID = "nomad-namespace"
+	NomadNodePool      LanguageID = "nomad-node-pool"
+	NomadResourceQuota LanguageID = "nomad-resource-quota"
+	NomadVariable      LanguageID = "nomad-variable"
+	NomadVolume        LanguageID = "nomad-volume"
 )
 
 func (l LanguageID) String() string {
