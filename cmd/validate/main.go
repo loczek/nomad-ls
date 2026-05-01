37a23700291efa8d8d3ad871b20f239f9eca1303
loczek <30776250+loczek@users.noreply.github.com>
2026-05-02 00:16:12 +0200

    refactor: validate all root schemas

--- cmd/validate/main.go
+++ cmd/validate/main.go
@@ -3,11 +3,51 @@ package main
 import (
 	"fmt"
 
-	"github.com/loczek/nomad-ls/internal/schema"
-	"github.com/loczek/nomad-ls/internal/schema/drivers"
+	schemaACL "github.com/loczek/nomad-ls/internal/schema/acl"
+	schemaAgent "github.com/loczek/nomad-ls/internal/schema/agent"
+	schema "github.com/loczek/nomad-ls/internal/schema/job"
+	"github.com/loczek/nomad-ls/internal/schema/job/drivers"
+	schemaNamespace "github.com/loczek/nomad-ls/internal/schema/namespace"
+	schemaNodePool "github.com/loczek/nomad-ls/internal/schema/node-pool"
+	schemaResourceQuota "github.com/loczek/nomad-ls/internal/schema/resource-quota"
+	schemaVariable "github.com/loczek/nomad-ls/internal/schema/variable"
+	"github.com/loczek/nomad-ls/internal/schema/volume/csi"
+	"github.com/loczek/nomad-ls/internal/schema/volume/dynamic"
 )
 
 func main() {
+	if err := schemaACL.RootSchema.Validate(); err != nil {
+		panic(fmt.Sprintf("ACL schema validation error: %s", err))
+	}
+	if err := schemaAgent.RootSchema.Validate(); err != nil {
+		panic(fmt.Sprintf("Agent schema validation error: %s", err))
+	}
+	if err := schema.RootBodySchema.Validate(); err != nil {
+		panic(fmt.Sprintf("Job schema validation failed: %s", err))
+	}
+	if err := schemaNamespace.RootSchema.Validate(); err != nil {
+		panic(fmt.Sprintf("Namespace schema validation error: %s", err))
+	}
+	if err := schemaNodePool.RootSchema.Validate(); err != nil {
+		panic(fmt.Sprintf("Nomad Pool schema validation error: %s", err))
+	}
+	if err := schemaResourceQuota.RootSchema.Validate(); err != nil {
+		panic(fmt.Sprintf("ResourceQuota schema validation error: %s", err))
+	}
+	if err := schemaVariable.RootSchema.Validate(); err != nil {
+		panic(fmt.Sprintf("Variable schema validation error: %s", err))
+	}
+	if err := csi.RootSchema.Validate(); err != nil {
+		panic(fmt.Sprintf("CSI Volume schema validation error: %s", err))
+	}
+	if err := dynamic.RootSchema.Validate(); err != nil {
+		panic(fmt.Sprintf("Dynamic Host Volume schema validation error: %s", err))
+	}
+
+	validateDrivers()
+}
+
+func validateDrivers() {
 	if err := drivers.DockerDriverSchema.Validate(); err != nil {
 		panic(fmt.Sprintf("Docker driver validation error: %s", err))
 	}
@@ -22,8 +62,5 @@ func main() {
 	}
 	if err := drivers.RawExecDriverSchema.Validate(); err != nil {
 		panic(fmt.Sprintf("RawExec driver validation error: %s", err))
-	}
-	if err := schema.RootBodySchema.Validate(); err != nil {
-		panic(fmt.Sprintf("Nomad Job schema validation error: %s", err))
 	}
 }
