2e09de014095d20efa2ce9344c0fb03d60cb7e73
loczek <30776250+loczek@users.noreply.github.com>
2026-05-02 00:09:10 +0200

    feat: add `node-pool` schema

---
+++ internal/schema/node-pool/schema.go
@@ -0,0 +1,65 @@
+package schemaNodePool
+
+import (
+	"github.com/hashicorp/hcl-lang/lang"
+	"github.com/hashicorp/hcl-lang/schema"
+	"github.com/zclconf/go-cty/cty"
+)
+
+var RootSchema = schema.BodySchema{
+	Blocks: map[string]*schema.BlockSchema{
+		"node_pool": {
+			Description: NodePoolSchema.Description,
+			Labels: []*schema.LabelSchema{
+				{Name: "name"},
+			},
+			Body: NodePoolSchema,
+		},
+	},
+}
+
+var NodePoolSchema = &schema.BodySchema{
+	Attributes: map[string]*schema.AttributeSchema{
+		"description": {
+			Description: lang.Markdown("Sets a human readable description for the node pool."),
+			Constraint:  schema.LiteralType{Type: cty.String},
+			IsOptional:  true,
+		},
+		"node_identity_ttl": {
+			Description: lang.Markdown("Sets the TTL for node identities issued to nodes in this node pool. The value must be a valid duration string (e.g. \"30m\", \"1h\", \"24h\"). If not set, the default value is \"24h\""),
+			Constraint:  schema.LiteralType{Type: cty.String},
+			IsOptional:  true,
+		},
+	},
+	Blocks: map[string]*schema.BlockSchema{
+		"meta": {
+			Body: &schema.BodySchema{
+				AnyAttribute: &schema.AttributeSchema{
+					Description: lang.Markdown("Sets optional metadata on the node pool, defined as key-value pairs. The scheduler does not use node pool metadata as part of scheduling."),
+					Constraint:  schema.LiteralType{Type: cty.String},
+					IsOptional:  true,
+				},
+			},
+		},
+		// TODO: mark as enterprise only
+		"scheduler_config": {
+			Description: SchedulerConfigSchema.Description,
+			Body:        SchedulerConfigSchema,
+		},
+	},
+}
+
+var SchedulerConfigSchema = &schema.BodySchema{
+	Attributes: map[string]*schema.AttributeSchema{
+		"description": {
+			Description: lang.Markdown("Sets a human readable description for the node pool."),
+			Constraint:  schema.LiteralType{Type: cty.String},
+			IsOptional:  true,
+		},
+		"node_identity_ttl": {
+			Description: lang.Markdown("Sets the TTL for node identities issued to nodes in this node pool. The value must be a valid duration string (e.g. \"30m\", \"1h\", \"24h\"). If not set, the default value is \"24h\""),
+			Constraint:  schema.LiteralType{Type: cty.String},
+			IsOptional:  true,
+		},
+	},
+}
