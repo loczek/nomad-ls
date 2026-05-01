0f7e4bd7df41cc6ecd5c6a4d89c17489678a6ec9
loczek <30776250+loczek@users.noreply.github.com>
2026-05-02 00:33:43 +0200

    refactor: update imports

--- cmd/validate/main.go
+++ cmd/validate/main.go
@@ -3,44 +3,36 @@ package main
 import (
 	"fmt"
 
-	schemaACL "github.com/loczek/nomad-ls/internal/schema/acl"
-	schemaAgent "github.com/loczek/nomad-ls/internal/schema/agent"
-	schema "github.com/loczek/nomad-ls/internal/schema/job"
+	"github.com/loczek/nomad-ls/internal/schema"
 	"github.com/loczek/nomad-ls/internal/schema/job/drivers"
-	schemaNamespace "github.com/loczek/nomad-ls/internal/schema/namespace"
-	schemaNodePool "github.com/loczek/nomad-ls/internal/schema/node-pool"
-	schemaResourceQuota "github.com/loczek/nomad-ls/internal/schema/resource-quota"
-	schemaVariable "github.com/loczek/nomad-ls/internal/schema/variable"
-	"github.com/loczek/nomad-ls/internal/schema/volume/csi"
-	"github.com/loczek/nomad-ls/internal/schema/volume/dynamic"
 )
 
 func main() {
-	if err := schemaACL.RootSchema.Validate(); err != nil {
+	if err := schema.NomadACL.Validate(); err != nil {
 		panic(fmt.Sprintf("ACL schema validation error: %s", err))
 	}
-	if err := schemaAgent.RootSchema.Validate(); err != nil {
+	if err := schema.NomadAgent.Validate(); err != nil {
 		panic(fmt.Sprintf("Agent schema validation error: %s", err))
 	}
-	if err := schema.RootBodySchema.Validate(); err != nil {
+	if err := schema.NomadJob.Validate(); err != nil {
 		panic(fmt.Sprintf("Job schema validation failed: %s", err))
 	}
-	if err := schemaNamespace.RootSchema.Validate(); err != nil {
+	if err := schema.NomadNamespace.Validate(); err != nil {
 		panic(fmt.Sprintf("Namespace schema validation error: %s", err))
 	}
-	if err := schemaNodePool.RootSchema.Validate(); err != nil {
+	if err := schema.NomadNodePool.Validate(); err != nil {
 		panic(fmt.Sprintf("Nomad Pool schema validation error: %s", err))
 	}
-	if err := schemaResourceQuota.RootSchema.Validate(); err != nil {
+	if err := schema.NomadResourceQuota.Validate(); err != nil {
 		panic(fmt.Sprintf("ResourceQuota schema validation error: %s", err))
 	}
-	if err := schemaVariable.RootSchema.Validate(); err != nil {
+	if err := schema.NomadVariable.Validate(); err != nil {
 		panic(fmt.Sprintf("Variable schema validation error: %s", err))
 	}
-	if err := csi.RootSchema.Validate(); err != nil {
+	if err := schema.NomadVolumeCSI.Validate(); err != nil {
 		panic(fmt.Sprintf("CSI Volume schema validation error: %s", err))
 	}
-	if err := dynamic.RootSchema.Validate(); err != nil {
+	if err := schema.NomadVolumeDynamic.Validate(); err != nil {
 		panic(fmt.Sprintf("Dynamic Host Volume schema validation error: %s", err))
 	}
 
--- internal/languages/schema.go
+++ internal/languages/schema.go
@@ -2,30 +2,24 @@ package languages
 
 import (
 	hclSchema "github.com/hashicorp/hcl-lang/schema"
-	schemaACL "github.com/loczek/nomad-ls/internal/schema/acl"
-	schemaJob "github.com/loczek/nomad-ls/internal/schema/job"
-	schemaNamespace "github.com/loczek/nomad-ls/internal/schema/namespace"
-	schemaNodePool "github.com/loczek/nomad-ls/internal/schema/node-pool"
-	schemaResourceQuota "github.com/loczek/nomad-ls/internal/schema/resource-quota"
-	schemaVariable "github.com/loczek/nomad-ls/internal/schema/variable"
-	schemaVolumeCSI "github.com/loczek/nomad-ls/internal/schema/volume/csi"
-	schemaVolumeDynamic "github.com/loczek/nomad-ls/internal/schema/volume/dynamic"
+	"github.com/loczek/nomad-ls/internal/schema"
 )
 
 var schemaMap = map[LanguageID]hclSchema.BodySchema{
-	NomadACL:           schemaACL.RootSchema,
-	NomadJob:           schemaJob.RootBodySchema, // TODO: rename this
-	NomadNapespace:     schemaNamespace.RootSchema,
-	NomadNodePool:      schemaNodePool.RootSchema,
-	NomadResourceQuota: schemaResourceQuota.RootSchema,
-	NomadVariable:      schemaVariable.RootSchema,
-	NomadVolumeCSI:     schemaVolumeCSI.RootSchema,
-	NomadVolumeDynamic: schemaVolumeDynamic.RootSchema,
+	NomadACL:           schema.NomadACL,
+	NomadAgent:         schema.NomadAgent,
+	NomadJob:           schema.NomadJob, // TODO: rename this
+	NomadNapespace:     schema.NomadNamespace,
+	NomadNodePool:      schema.NomadNodePool,
+	NomadResourceQuota: schema.NomadResourceQuota,
+	NomadVariable:      schema.NomadVariable,
+	NomadVolumeCSI:     schema.NomadVolumeCSI,
+	NomadVolumeDynamic: schema.NomadVolumeDynamic,
 }
 
 func ToSchema(lang LanguageID) hclSchema.BodySchema {
 	if schema, ok := schemaMap[lang]; ok {
 		return schema
 	}
-	return schemaJob.RootBodySchema
+	return schema.NomadJob
 }
