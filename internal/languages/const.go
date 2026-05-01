f2a18a1bf9123c2451975680676ad236a918186b
loczek <30776250+loczek@users.noreply.github.com>
2026-05-02 00:13:00 +0200

    feat: add languages

--- internal/languages/const.go
+++ internal/languages/const.go
@@ -1,18 +1,43 @@
 package languages
 
+import (
+	"fmt"
+)
+
 type LanguageID string
 
 const (
-	NomadJob           LanguageID = "nomad-job"
-	NomadAgent         LanguageID = "nomad-agent"
 	NomadACL           LanguageID = "nomad-acl"
+	NomadAgent         LanguageID = "nomad-agent"
+	NomadJob           LanguageID = "nomad-job"
 	NomadNapespace     LanguageID = "nomad-namespace"
 	NomadNodePool      LanguageID = "nomad-node-pool"
 	NomadResourceQuota LanguageID = "nomad-resource-quota"
 	NomadVariable      LanguageID = "nomad-variable"
-	NomadVolume        LanguageID = "nomad-volume"
+	NomadVolumeCSI     LanguageID = "nomad-volume-csi"
+	NomadVolumeDynamic LanguageID = "nomad-volume-dynamic"
 )
+
+var langs = map[string]LanguageID{
+	"nomad-acl":            NomadACL,
+	"nomad-agent":          NomadAgent,
+	"nomad-job":            NomadJob,
+	"nomad-namespace":      NomadNapespace,
+	"nomad-node-pool":      NomadNodePool,
+	"nomad-resource-quota": NomadResourceQuota,
+	"nomad-variable":       NomadVariable,
+	"nomad-volume-csi":     NomadVolumeCSI,
+	"nomad-volume-dynamic": NomadVolumeDynamic,
+}
 
 func (l LanguageID) String() string {
 	return string(l)
+}
+
+func NewFromString(id string) (LanguageID, error) {
+	if val, ok := langs[id]; ok {
+		return val, nil
+	}
+
+	return "", fmt.Errorf("LanguageID: \"%s\" is not a valid language id", id)
 }
