4d8f47a79662ae276dcc63e6335f858fecdbc009
loczek <30776250+loczek@users.noreply.github.com>
2026-05-27 22:37:32 +0200

    fix: add `raft_boltdb` block body schema (#22)
    * fix: add `raft_boltdb` block body schema
    
    * fix: mark as deprecated

--- internal/schema/agent/server.go
+++ internal/schema/agent/server.go
@@ -292,7 +292,8 @@ var ServerSchema = &schema.BodySchema{
 			Body:        PlanRejectionTrackerSchema,
 		},
 		"raft_boltdb": {
-			Description:  lang.Markdown("This is a nested object that allows configuring options for Raft's BoltDB based log store."),
+			Description: lang.Markdown("This is a nested object that allows configuring options for Raft's BoltDB based log store."),
+			Body:         RaftBoltDBSchema,
 			IsDeprecated: true,
 		},
 		"raft_logstore": {
@@ -423,6 +424,18 @@ var PlanRejectionTrackerSchema = &schema.BodySchema{
 			DefaultValue: schema.DefaultValue{Value: cty.StringVal("5m")},
 			Constraint:   schema.LiteralType{Type: cty.String},
 			IsOptional:   true,
+		},
+	},
+}
+
+var RaftBoltDBSchema = &schema.BodySchema{
+	Attributes: map[string]*schema.AttributeSchema{
+		"no_freelist_sync": {
+			Description:  lang.Markdown("Setting this to true will disable syncing the BoltDB freelist to disk within the raft.db file. Not syncing the freelist to disk will reduce disk IO required for write operations at the expense of longer server startup times."),
+			DefaultValue: schema.DefaultValue{Value: cty.BoolVal(false)},
+			Constraint:   schema.LiteralType{Type: cty.Bool},
+			IsOptional:   true,
+			IsDeprecated: true,
 		},
 	},
 }
